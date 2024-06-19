778ed9f9b5f1949de42b95887f4a3cbcf0256726
CodedSakura <me@codedsakura.dev>
2024-06-20 00:50:20 +0300

    add option to PlaySound to accept null sound

--- src/main/java/dev/codedsakura/blossom/lib/polyfill/PlaySound.java
+++ src/main/java/dev/codedsakura/blossom/lib/polyfill/PlaySound.java
@@ -3,6 +3,7 @@ package dev.codedsakura.blossom.lib.polyfill;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
+import org.jetbrains.annotations.Nullable;
 
 public class PlaySound {
     public static void playToPlayer(ServerPlayerEntity player, Sound sound) {
@@ -12,6 +13,12 @@ public class PlaySound {
                 sound.volume(),
                 sound.pitch()
         );
+    }
+
+    public static void maybePlayToPlayer(ServerPlayerEntity player, @Nullable Sound sound) {
+        if (sound != null) {
+            playToPlayer(player, sound);
+        }
     }
 
     public record Sound(String id, float volume, float pitch) {
